426b42047caf08839106f30a5397f6f2d5cadd8b
Pauline <pauline.s23@outlook.fr>
2025-02-06 11:51:43 +0100

    new fonctionalities

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor() {}
+
+  searchQuery: string = '';
+
+  constructor(private searchService: SearchService) {}
+
+  onSearchChange() {
+    this.searchService.updateSearchQuery(this.searchQuery);
+  }
+
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product.model';
 import { NgModel } from '@angular/forms';
+import { SearchService } from '../services/search.service';
 
 @Component({
   selector: 'app-products',
@@ -10,6 +11,8 @@ import { NgModel } from '@angular/forms';
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = []; // Liste des produits
+  filteredProducts: Product[] = [];
+
   sortOptions = [
     { value: 'priceAsc', label: 'Prix croissant' },
     { value: 'priceDesc', label: 'Prix décroissant' },
@@ -18,28 +21,56 @@ export class ProductsComponent implements OnInit {
   ];
   selectedSort: string = 'nameAsc';
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private searchService: SearchService
+  ) {}
 
   ngOnInit(): void {
-    this.selectedSort = 'nameAsc';
-    //this.productService.getProducts();
-    this.getShoes();
+    this.selectedSort = 'nameAsc'; //Par défaut les produits sont affichés par ordre alphabétique
+    this.getShoes(); //Charger initialement les produits
+    this.searchService.currentSearchQuery.subscribe(query => {
+      console.log('Recherche en cours :', query);
+      this.filterProducts(query);
+    });
+  }
+  filterProducts(query: string): void {
+    const lowerQuery = query.toLowerCase();
+    if (lowerQuery) {
+      this.filteredProducts = this.products.filter(product =>
+        product.name.toLowerCase().includes(lowerQuery)
+      );
+    } else {
+      this.filteredProducts = [...this.products]; //Affiche tout si pas de recherche
+    }
   }
 
 
   getShoes(): void {
     switch (this.selectedSort) {
       case 'priceAsc':
-        this.productService.getShoesByPriceAsc().subscribe((data: Product[]) => {this.products = data});
+        this.productService.getShoesByPriceAsc().subscribe((data: Product[]) => {
+          this.products = data;
+          this.filteredProducts = [...this.products]; //Initialisation du filtrage
+        });
         break;
       case 'priceDesc':
-        this.productService.getShoesByPriceDesc().subscribe((data: Product[]) => {this.products = data});
+        this.productService.getShoesByPriceDesc().subscribe((data: Product[]) => {
+          this.products = data;
+          this.filteredProducts = [...this.products];
+        });
         break;
       case 'nameAsc':
-        this.productService.getShoesByNameAsc().subscribe((data: Product[]) => {this.products = data});
+        this.productService.getShoesByNameAsc().subscribe((data: Product[]) => {
+          this.products = data;
+          this.filteredProducts = [...this.products];
+        });
         break;
       case 'nameDesc':
-        this.productService.getShoesByNameDesc().subscribe((data: Product[]) => {this.products = data});
+        this.productService.getShoesByNameDesc().subscribe((data: Product[]) => {
+          this.products = data;
+          this.filteredProducts = [...this.products];
+        });
         break;
     }
   }
